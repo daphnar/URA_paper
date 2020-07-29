c8d8ec2cde614e49802d58dca07b3280e965595f
daphnar <aaaa85Al>
2020-07-30 01:18:36 +0300

    rfgd

--- Figures/util_for_figure4.py
+++ Figures/util_for_figure4.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
 def merger(pheno):
-    obj1 = pd.read_csv('saturation_xgboostMB_%s_vs_ILval.csv'%pheno)
-    obj2 = pd.read_csv('saturation_RidgeMB_%s_vs_ILval.csv'%pheno)
+    obj1 = pd.read_csv('saturation_xgboostMB_%s_vs_US.csv'%pheno)
+    obj2 = pd.read_csv('saturation_RidgeMB_%s_vs_US.csv'%pheno)
     merged = obj1.merge(obj2,right_index=True,left_index=True)
     merged.columns = [0,'cohort_size','mean_pearson','mean_std',1,2,'mean_pearson_linear','mean_std_linear']
     merged = merged[['cohort_size','mean_pearson','mean_std','mean_pearson_linear','mean_std_linear']]
-    merged.to_csv('Figures - %s_saturation2.csv'%pheno)
+    merged.to_csv('Figures - %s_saturation_US.csv'%pheno)
 
+merger('age')
 merger('hba1c')
 merger('bmi')
---
+++ Figures/util_for_figure4andS1.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import os
+basepath='/net/mraid08/export/jafar/Microbiome/Analyses/Unicorn/Cohort_Paper/revision_Analyses/figures'
+
+phenotypes = ['age','bmi','hba1c',"bt__fasting_glucose","bt__fasting_triglycerides",'bowel_movement_frequency',
+              "bt__hdl_cholesterol","bt__cholesterol","bt__protein","bt__albumin","bt__sgot","bt__alkaline_phosphatase",
+              "bt__tsh","bt__inr",'gender','currently_smokes','ever_smoked','type_2_diabetes']
+
+def model_quantitive_rename(df):
+    return df.rename(index=str,columns={'mean_pearson':'pearson_linear','std_pearson':'stdev_linear'})
+
+def model_binary_rename(gender,df):
+    if gender != 'all':
+        return pd.DataFrame({'target':phenotypes})
+    return df.rename(index=str,columns={'auc':'auc_linear','stdev':'stdev_linear'})\
+        [['target','n','auc_linear','stdev_linear']]
+
+def gender_quantitive_rename(gender, df):
+    if gender=='all':
+        new_df = df.rename(index=str,columns={'cohort_size':'n',
+                     'mean_pearson':'pearson','std_pearson':'stdev'})
+    elif gender=='female':
+        new_df = df.rename(index=str,columns={'cohort_size':'n_female',
+                    'mean_pearson':'pearson_female','std_pearson':'stdev_female'})
+    elif gender=='male':
+        new_df = df.rename(index=str,columns={'cohort_size':'n_male',
+                    'mean_pearson':'pearson_male','std_pearson':'stdev_male'})
+    return new_df
+
+def gender_binary_rename(gender, df):
+    if gender == 'all':
+        new_df = df[['target','auc','stdev']]
+    elif gender=='female':
+        new_df = df.rename(index=str,columns={'auc':'pearson_female','stdev':'stdev_female'})\
+        [['target','pearson_female','stdev_female']]
+    elif gender=='male':
+        new_df = df.rename(index=str,columns={'auc':'pearson_male','stdev':'stdev_male'}) \
+            [['target','pearson_male', 'stdev_male']]
+    return new_df
+
+def merger_quantitive():
+    obj_ridge = pd.read_csv(os.path.join(basepath,'prediction_Ridge_linear.csv'),index_col=0)
+    dfs=[]
+    obj_xgboost = pd.read_csv(os.path.join(basepath,'prediction_xgboostMB_gender.csv'),index_col=0)
+    dfs.append(model_quantitive_rename(obj_ridge))
+    for gender,df in obj_xgboost.groupby('gender'):
+        dfs.append(gender_quantitive_rename(gender,df))
+    merge_on_target(dfs,os.path.join(basepath, 'Figures - prediction_other_phenotypes.csv'))
+
+def merger_binary():
+    obj_ridge = pd.read_csv(os.path.join(basepath,'classification_sgdMB_phenotypes.csv'),index_col=0)
+    dfs=[]
+    obj_xgboost = pd.read_csv(os.path.join(basepath,'classification_xgboostMB_phenotypes.csv'),index_col=0)
+    for gender, df in obj_ridge.groupby('gender'):
+        dfs.append(model_binary_rename(gender,obj_ridge))
+    for gender,df in obj_xgboost.groupby('gender'):
+        dfs.append(gender_binary_rename(gender,df))
+    merge_on_target(dfs,os.path.join(basepath, 'Figures - classification.csv'))
+
+
+def merge_on_target(dfs,output):
+    merged = pd.DataFrame({'target':phenotypes})
+    for df in dfs:
+        print(merged)
+        merged = merged.merge(df,on='target')
+    merged.to_csv(output)
+
+#merger_quantitive()
+merger_binary()
